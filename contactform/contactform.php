58f9bdd3517916677be570b3d1613af79f7b01ba
leongshengmin <leongshengmin@gmail.com>
2019-04-07 21:01:21 +0800

    update contactform php

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -4,51 +4,12 @@ if(isset($_POST['email'])) {
     // EDIT THE 2 LINES BELOW AS REQUIRED
     $email_to = "shengmin@u.nus.edu";
  
-    function died($error) {
-        // your error code can go here
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
-        echo $error."<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
-        die();
-    }
- 
- 
-    // validation expected data exists
-    if(!isset($_POST['name']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['message'])) {
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
-    }
- 
-     
- 
     $name = $_POST['name']; // required
     $email_from = $_POST['email']; // required
     $subject = $_POST['subject']; // not required
     $message = $_POST['message']; // required
  
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'The email address you entered does not appear to be valid.<br />';
-  }
- 
-    $string_exp = "/^[A-Za-z .'-]+$/";
- 
-  if(!preg_match($string_exp,$name)) {
-    $error_message .= 'The name you entered does not appear to be valid.<br />';
-  }
- 
-  if(strlen($message) < 2) {
-    $error_message .= 'The message you entered does not appear to be valid.<br />';
-  }
- 
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
- 
+    
     $email_message = "Form details below.\n\n";
  
      
@@ -63,6 +24,8 @@ if(isset($_POST['email'])) {
     $email_message .= "Email: ".clean_string($email_from)."\n";
     $email_message .= "Subject: ".clean_string($subject)."\n";
     $email_message .= "Message: ".clean_string($message)."\n";
+
+	echo($email_message);
  
 // create email headers
 $headers = 'From: '.$email_from."\r\n".
