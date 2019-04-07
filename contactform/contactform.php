6ae1e68a053bfce697fa36b0cc9c51853ba196ff
leongshengmin <leongshengmin@gmail.com>
2019-04-08 06:46:19 +0800

    php upd

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,17 +1,28 @@
 <?php
-if(isset($_POST['email'])) {
- 
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//SMTP needs accurate times, and the PHP time zone MUST be set
+//This should be done in your php.ini, but this is how to do it if you don't have access to that
+date_default_timezone_set('Etc/UTC');
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+
     // EDIT THE 2 LINES BELOW AS REQUIRED
     $email_to = "e0235265@u.nus.edu";
  
     $name = $_POST['name']; // required
     $email_from = $_POST['email']; // required
     $subject = $_POST['subject']; // not required
-    $message = $_POST['message']; // required
- 
-    
-    $email_message = "Form details below.\n\n";
- 
+    $message = $_POST['message']; // required 
      
     function clean_string($string) {
       $bad = array("content-type","bcc:","to:","cc:","href");
@@ -22,26 +33,30 @@ if(isset($_POST['email'])) {
     $email_message .= "Email: ".clean_string($email_from)."\n";
     $email_message .= "Subject: ".clean_string($subject)."\n";
     $email_message .= "Message: ".clean_string($message)."\n";
- 
-// create email headers
-$headers  = 'MIME-Version: 1.0' . "\r\n"
-    .'Content-type: text/html; charset=utf-8' . "\r\n"
-    .'From: '. 'leongshengmin@herokuapp.com/' . "\r\n";
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-if (@mail($email_to, $subject, $email_message, $headers)) {
-  echo("done sending");
-} else {
-  echo("error sending message");
-}
+    
+    // create email headers
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+        .'Content-type: text/html; charset=utf-8' . "\r\n"
+        .'From: '. 'leongshengmin@herokuapp.com/' . "\r\n";
+    'Reply-To: '.$email_from."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+    //Server settings
+    $mail->SMTPDebug = 2;                                       // Enable verbose debug output
+    $mail->isSMTP();                                            // Set mailer to use SMTP
+    $mail->Host       = $email_from;  // Specify main and backup SMTP servers
+    $mail->Port       = 25;                                    // TCP port to connect to
 
-?>
- 
-<!-- include your own success html here -->
- 
-Thank you for contacting me. I will be in touch with you shortly.
- 
-<?php
- 
+    //Recipients
+    $mail->setFrom($email_from, $name);
+    //Set who the message is to be sent to
+    $mail->addAddress($email_to, 'Receiver');
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = $subject;
+    $mail->Body    = $email_message;
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-?>
